e603a2e19bcd912a9198f6df8a3c2ceb31e9840b
Athu5 <84505685+Athu5@users.noreply.github.com>
2023-05-30 19:55:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTCI2ieg51ZuDRoj6CiFqz_zkHgiHqUMM",
+  authDomain: "pro-67-ceb96.firebaseapp.com",
+  projectId: "pro-67-ceb96",
+  storageBucket: "pro-67-ceb96.appspot.com",
+  messagingSenderId: "509532873351",
+  appId: "1:509532873351:web:d48aabdc17571a88e08e22"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-export default firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
